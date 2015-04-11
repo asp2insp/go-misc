390e1cb8197bff216df49965f92c340f1e198ddd
Josiah Gaskin <josiahgaskin@gmail.com>
2015-04-10 20:13:46 -0700

    updated gitignore to remore ds store

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,15 @@
 package utils
 
+import "os"
+
 func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func Filesize(f *os.File) uint64 {
+	fileInfo, err := f.Stat()
+	Check(err)
+	return uint64(fileInfo.Size())
 }
