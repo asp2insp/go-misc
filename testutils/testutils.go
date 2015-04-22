1daab6e6750a6ca090f013a243b0d2074a0c7bef
Josiah Gaskin <josiahgaskin@gmail.com>
2015-04-21 23:02:15 -0700

    Added more testutils

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -20,6 +20,12 @@ func CheckInt64(exp, obs int64, t *testing.T) {
 	}
 }
 
+func CheckUint(exp, obs uint, t *testing.T) {
+	if exp != obs {
+		t.Errorf("Expected %v but got %v", exp, obs)
+	}
+}
+
 func CheckUint64(exp, obs uint64, t *testing.T) {
 	if exp != obs {
 		t.Errorf("Expected %v but got %v", exp, obs)
@@ -38,6 +44,12 @@ func ExpectFalse(obs bool, message string, t *testing.T) {
 	}
 }
 
+func ExpectNil(obs interface{}, message string, t *testing.T) {
+	if obs != nil {
+		t.Errorf("Expected nil but got: %v. (%s)", obs, message)
+	}
+}
+
 func CheckErr(err error, t *testing.T) {
 	if err != nil {
 		t.Error(err.Error())
@@ -51,6 +63,30 @@ func CheckSlice(exp, obs []int, t *testing.T) {
 	for i, v := range exp {
 		if v != obs[i] {
 			t.Errorf("Slices differ in element number %d: %d vs %d", i, v, obs[i])
+		}
+	}
+	t.Logf("Expected: %v\nActual: %v", exp, obs)
+}
+
+func CheckByteSlice(exp, obs []byte, t *testing.T) {
+	if len(exp) != len(obs) {
+		t.Errorf("Differing slice lengths: %d vs %d", len(exp), len(obs))
+	}
+	for i, v := range exp {
+		if v != obs[i] {
+			t.Errorf("Slices differ in element number %d: %d vs %d", i, v, obs[i])
+		}
+	}
+	t.Logf("Expected: %v\nActual: %v", exp, obs)
+}
+
+func CheckStringSlice(exp, obs []string, t *testing.T) {
+	if len(exp) != len(obs) {
+		t.Errorf("Differing slice lengths: %d vs %d", len(exp), len(obs))
+	}
+	for i, v := range exp {
+		if v != obs[i] {
+			t.Errorf("Slices differ in element number %d: %v vs %v", i, v, obs[i])
 		}
 	}
 	t.Logf("Expected: %v\nActual: %v", exp, obs)
